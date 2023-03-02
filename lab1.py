9ae77cb68be92ac73ebfe5d19486775579c99f80
Andreas Rebutsikas <up1072800@upnet.gr>
2023-03-02 19:53:24 +0200

    second question

--- lab1.py
+++ lab1.py
@@ -5,3 +5,18 @@ re = requests.get(a)
 print("\nHeaders: \n")
 for i in re.headers :
     print (i)
+print("\nServer: " + re.headers['Server'])
+if('Set-Cookie'in re.headers):
+    print("\nThe URL Cookies: \n")
+    for j in re.cookies:
+        print(j.name)
+        if j.expires==None:
+            print("Does Not Expire\n")
+        else:
+            ed=float(j.expires)
+            nd=datetime.now().timestamp()
+            print(" Expires In: ")
+            print(datetime.fromtimestamp(ed)-datetime.fromtimestamp(nd))
+            print("\n")
+else:
+    print("No Cookie")
